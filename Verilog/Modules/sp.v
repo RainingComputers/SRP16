ec6fece1107f33de9affce8004ac20b7748bc808
RainingComputers <vishnu.vish.shankar@gmail.com>
2019-10-13 19:39:58 +0530

    Removed unused wire

--- Verilog/Modules/sp.v
+++ Verilog/Modules/sp.v
@@ -3,7 +3,7 @@ module sp(din, read_dbus, write, pop, push, clk,
 );
     /* Input ports */
     input [15:0] din;
-    input read_abus, read_dbus, write, pop, push, clk, reset;
+    input read_dbus, write, pop, push, clk, reset;
 
     /* Output ports */
     output [15:0] abus_out;
@@ -14,13 +14,13 @@ module sp(din, read_dbus, write, pop, push, clk,
     reg push_flag;
     reg pop_flag;
     
-    /* If read_abus or pop is high, abus_out[15:0] will be data[15:0] */
+    /* If pop is high, abus_out[15:0] will be data[15:0] */
     assign abus_out = (pop)? data : 16'hzzzz;
 
     /* If push, abus_out[15:0] will be data[15:0]-16'h0001 */
     assign abus_out = (push)? data - 16'h0001 :  16'hzzzz;
 
-    /* If read_abus is high, abus_out[15:0] will be data[15:0] */
+    /* If read_dbus is high, dbus_out[15:0] will be data[15:0] */
     assign dbus_out = (read_dbus)? data : 16'hzzzz;
 
     /* When there is a positive edge on the clock */
@@ -50,7 +50,6 @@ module sp(din, read_dbus, write, pop, push, clk,
         end
     end
 
-    /* When there is a positive edge on the read_abus line */
     /* Increment or Decrement register based on the flags */
     always @ (posedge pop, posedge push)
     begin
