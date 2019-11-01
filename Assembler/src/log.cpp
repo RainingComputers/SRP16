66566c4df96fc73fe62a45df10da09ae2f547f9b
RainingComputers <vishnu.vish.shankar@gmail.com>
2019-11-01 19:03:06 +0530

    Added const where needed

--- Assembler/src/log.cpp
+++ Assembler/src/log.cpp
@@ -2,20 +2,20 @@
 
 namespace log
 {
-    static void in_file(std::string& file_name)
+    static void in_file(const std::string& file_name)
     {
         std::cout<<"In file \""<<file_name<<"\":\n";
     }
 
     void syntax_error(const std::string& error, int line_no, 
-        std::string&file_name)
+        const std::string&file_name)
     {
         in_file(file_name);
         std::cout<<"SYNTAX-ERROR: At line "<<line_no<<", "<<error<<"\n";
     }
 
     void operand_error(const std::string& error, int line_no,
-        std::string&file_name)
+        const std::string&file_name)
     {
         in_file(file_name);
         std::cout<<"OPERAND-ERROR: At line "<<line_no<<", "<<error<<"\n";
@@ -32,7 +32,7 @@ namespace log
     }
 
     void include_error(const std::string& inc_file_name, int line_no, 
-        std::string& file_name)
+        const std::string& file_name)
     {
         in_file(file_name);
         std::cout<<"INCLUDE: Unable to open file \""<<inc_file_name<<"\""<<"\n";
--- Assembler/src/log.hpp
+++ Assembler/src/log.hpp
@@ -7,13 +7,13 @@
 namespace log
 {
     void syntax_error(const std::string& error, int line_no, 
-        std::string&file_name);
+        const std::string&file_name);
     void operand_error(const std::string& error, int line_no,
-        std::string&file_name);
+        const std::string&file_name);
     void error(const std::string& error);
     void usage_error(const std::string& error);
     void include_error(const std::string& inc_file_name, int line_no, 
-        std::string& file_name);
+        const std::string& file_name);
 }
 
 
